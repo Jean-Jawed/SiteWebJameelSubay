3231da372ce4f2ee6f2ae328fc35730fbb8d230e
Javed <jawed_tahir@yahoo.fr>
2025-05-28 16:18:18 +0200

    Config Vite base URL pour la GithubPages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/SiteWebJameelSubay/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
